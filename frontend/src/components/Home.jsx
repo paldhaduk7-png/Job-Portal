66dbb540103ff88fde9c16847dbd19323532c61c
pal <paldhaduk7@gmail.com>
2026-05-09 23:49:13 +0530

    fix filter logic in job

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,16 +4,25 @@ import HeroSection from './HeroSection'
 import LatestJobs from './LatestJobs'
 import Footer from './Fotter'
 import useGetAllJobs from '@/hooks/useGetAllJobs'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { setSearchQuery } from '@/redux/jobSlice' 
 
 const Home = () => {
 useGetAllJobs();
 
+ const dispatch = useDispatch(); 
 const {user}=useSelector(store=>store.auth);
 // console.log(user);
 const navigate=useNavigate();
  
+  useEffect(() => {
+    // clear search query when home page loads
+    dispatch(setSearchQuery(""));  // ← add this
+  }, []);
+
+
+
 useEffect ( ()=>{
   if(user?.role=== 'recruiter'){
 navigate("/admin/companies")
