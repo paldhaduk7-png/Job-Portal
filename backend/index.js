737314bbeb36cb499c8079722cb204de80636310
pal <paldhaduk7@gmail.com>
2026-08-01 16:54:11 +0530

    Add save job backend feature

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ import connectDB from "./utils/db.js";
 import userRoute from  "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js"
+import applicationRoute from "./routes/application.route.js";
+import saveRoute from "./routes/savedJob.route.js";
 
 const app=express();
 
@@ -27,6 +28,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
+app.use("/api/v1/save", saveRoute);
 
 app.listen(PORT, ()=>{
     console.log(`server is satrt at port ${PORT}`);
