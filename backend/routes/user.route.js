628c249d0738b24e7718fa3311ca969cba4ade17
pal <paldhaduk7@gmail.com>
2026-08-04 01:48:27 +0530

    Added forgot password backend with OTP email support

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register , Login ,logout,updateProfile,updateRecruiterProfile} from "../controllers/user.controller.js";
+import { register , Login ,logout,updateProfile,updateRecruiterProfile , forgotPassword,verifyOTP,resetPassword} from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthentication.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -11,7 +11,9 @@ router.post("/login", Login);
 router.post("/logout", logout);
 router.post("/profile/update",  isAuthenticated,singleUpload, updateProfile);
 router.post("/profile/recruiter/update",  isAuthenticated,singleUpload,updateRecruiterProfile);
-
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOTP);
+router.post("/reset-password", resetPassword);
 export default router; 
 
 
